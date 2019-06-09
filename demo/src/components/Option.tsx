05207431ea429871f14ecbd623e44c396289c6f5
DomParfitt <dom.parfitt@gmail.com>
2019-06-09 18:41:58 +0100

    Added individual Option component

---
+++ demo/src/components/Option.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+export class Option extends React.Component<OptionProps, any> {
+
+    constructor(props: OptionProps) {
+        super(props);
+    }
+
+    public render() {
+        return (
+            <tr >
+                <td>{this.props.name}</td>
+                <td>
+                    {this.getInputElement()}
+                </td>
+            </tr>
+        );
+    }
+
+    private getInputElement = () => {
+        switch (this.props.type) {
+            case 'boolean':
+            case 'number':
+            case 'input':
+                return <input
+                    type={this.props.type === 'boolean' ? 'checkbox' : this.props.type}
+                    onChange={this.getOnUpdate()} />;
+            case 'list':
+                const opts: JSX.Element[] = [];
+                (this.props.values || []).forEach((value, index) => {
+                    opts.push(
+                        <option key={index} value={value}>{value}</option>
+                    );
+                });
+                //@ts-ignore
+                return <select onChange={this.getOnUpdate()}>{opts}</select>
+            default:
+                return <div />
+        }
+    }
+
+    private getOnUpdate = () => {
+        if (this.props.type === 'boolean') {
+            return (event: React.ChangeEvent<HTMLInputElement>) => this.props.onOptionUpdate(this.props.name, event.target.checked);
+        } else {
+            return (event: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement>) => this.props.onOptionUpdate(this.props.name, event.target.value);
+        }
+    }
+
+}
+
+export interface OptionProps {
+    name: string,
+    type: 'boolean' | 'number' | 'input' | 'list',
+    values?: string[],
+    default?: any,
+    onOptionUpdate: (name: string, value: any) => void,
+}
