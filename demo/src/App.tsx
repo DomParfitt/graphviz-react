075f2e7be312d1f76835498f622b8099dd7c24ac
DomParfitt <dom.parfitt@gmail.com>
2019-06-09 18:10:42 +0100

    Added OptionsSelector details

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -3,8 +3,26 @@ import React from 'react';
 
 
 import { Graphviz } from 'graphviz-react';
-import { OptionsSelector } from './components/OptionSelector';
+import { OptionsSelector, Option } from './components/OptionSelector';
 import { GraphInput } from './components/GraphInput';
+
+const options: Option[] = [
+  { name: 'useWorker', type: 'boolean' },
+  { name: 'engine', type: 'list', values: ['circo', 'dot', 'fdp', 'neato', 'osage', 'patchwork', 'twopi'] },
+  { name: 'totalMemory', type: 'number' },
+  { name: 'keyMode', type: 'list', values: ['title', 'id', 'tag-index', 'index'] },
+  { name: 'fade', type: 'boolean' },
+  { name: 'tweenPaths', type: 'boolean' },
+  { name: 'tweenShapes', type: 'boolean' },
+  { name: 'convertEqualSidedPolygons', type: 'boolean' },
+  { name: 'tweenPrecision', type: 'number' },
+  { name: 'growEnteringEdges', type: 'boolean' },
+  { name: 'zoom', type: 'boolean' },
+  { name: 'width', type: 'number' },
+  { name: 'height', type: 'number' },
+  { name: 'scale', type: 'number' },
+  { name: 'fit', type: 'boolean' },
+];
 
 export default class App extends React.Component<any, AppState> {
 
@@ -16,23 +34,35 @@ export default class App extends React.Component<any, AppState> {
     };
   }
 
-  render(): JSX.Element {
+  public componentDidUpdate() {
+    console.log(JSON.stringify(this.state.graphOptions));
+  }
+
+  public render(): JSX.Element {
     return (
       <div>
         <h1>Graphviz-React</h1>
         <table>
-          <tr>
-            <td>
-              <GraphInput initial={this.state.dot} onUpdate={(dot) => this.setState({dot})} />
-              <OptionsSelector />
-            </td>
-            <td>
-              <Graphviz dot={this.state.dot} options={this.state.graphOptions}/>
-            </td>
-          </tr>
+          <tbody>
+            <tr>
+              <td>
+                <GraphInput initial={this.state.dot} onUpdate={(dot) => this.setState({ dot })} />
+                <OptionsSelector options={options} onOptionUpdate={this.onOptionUpdate} />
+              </td>
+              <td>
+                <Graphviz dot={this.state.dot} options={this.state.graphOptions} />
+              </td>
+            </tr>
+          </tbody>
         </table>
       </div>
     );
+  }
+
+  private onOptionUpdate = (name: string, value: any) => {
+    const graphOptions = { ...this.state.graphOptions };
+    graphOptions[name] = value;
+    this.setState({ graphOptions });
   }
 }
 
