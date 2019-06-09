47f6e26c48a3cb7b0c67fec9ecac8c911df669a5
DomParfitt <dom.parfitt@gmail.com>
2019-06-09 18:42:08 +0100

    Updated to use Option component

--- demo/src/components/OptionSelector.tsx
+++ demo/src/components/OptionSelector.tsx
@@ -1,59 +1,22 @@
 import React from "react";
+import { Option } from "./Option";
 
 export class OptionsSelector extends React.Component<OptionsSelectorProps, any> {
 
     public render() {
         const items: JSX.Element[] = [];
         this.props.options.forEach((option, index) => {
-            let input: JSX.Element =
-                <input
-                    key={index}
-                    type="checkbox"
-                    onChange={(event) => this.props.onOptionUpdate(option.name, event.target.checked)}
-                />;
-
-            switch (option.type) {
-                case 'number':
-                    input =
-                        <input
-                            key={index}
-                            type="number"
-                            onChange={(event) => this.props.onOptionUpdate(option.name, event.target.value)}
-                        />;
-                    break;
-                case 'input':
-                    input =
-                        <input
-                            key={index}
-                            type="input"
-                            onChange={(event) => this.props.onOptionUpdate(option.name, event.target.value)}
-                        />;
-                    break;
-                case 'list':
-                    const opts: JSX.Element[] = [];
-                    option.values!.forEach((value, index) => {
-                        opts.push(
-                            <option key={index} value={value}>{value}</option>
-                        );
-                    });
-                    input = <select
-                        key={index}
-                        onChange={(event) => this.props.onOptionUpdate(option.name, event.target.value)}>
-                        {opts}</select>
-                    break;
-                default:
-                    break;
-            }
 
             items.push(
-                <tr key={index}>
-                    <td>{option.name}</td>
-                    <td>
-                        {input}
-                    </td>
-                </tr>
+                <Option 
+                    key={index}
+                    name={option.name} 
+                    type={option.type}
+                    values={option.values}
+                    onOptionUpdate={this.props.onOptionUpdate} />
             );
         });
+
         return (
             <div>
                 <table>
@@ -67,11 +30,11 @@ export class OptionsSelector extends React.Component<OptionsSelectorProps, any> 
 }
 
 export interface OptionsSelectorProps {
-    options: Option[],
+    options: IOption[],
     onOptionUpdate: (name: string, value: any) => void,
 }
 
-export interface Option {
+export interface IOption {
     name: string,
     type: 'boolean' | 'number' | 'input' | 'list',
     values?: string[],
