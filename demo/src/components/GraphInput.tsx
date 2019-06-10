d41cc54cc901f07fba6e08fb2f1b5a5c0fc1e872
DomParfitt <dominicparfitt@economist.com>
2019-06-10 13:02:50 +0100

    Added optional auto-updating of graph

--- demo/src/components/GraphInput.tsx
+++ demo/src/components/GraphInput.tsx
@@ -6,6 +6,7 @@ export class GraphInput extends React.Component<GraphInputProps, GraphInputState
   constructor(props: any) {
     super(props);
     this.state = {
+      autoUpdate: false,
       dot: this.props.initial || '',
       error: '',
       temp: this.props.initial || '',
@@ -15,19 +16,34 @@ export class GraphInput extends React.Component<GraphInputProps, GraphInputState
   public render() {
     return (
       <div className="graph-input">
-        <textarea defaultValue={this.state.dot} onChange={(event) => {
-          this.setState({ temp: event.target.value });
-        }} />
+        <textarea defaultValue={this.state.dot} onChange={this.onChange} />
         <div style={{ color: 'red' }}>{this.state.error}</div>
+        <label>Auto-update?</label>
+        <input type='checkbox' onChange={
+          (event) => {
+            this.setState({ autoUpdate: event.target.checked });
+          }
+        } />
         <button onClick={this.onClick}>Update</button>
       </div>
     );
   }
 
+  private onChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    this.setState({ temp: event.target.value });
+    if (this.state.autoUpdate) {
+      this.updateGraph(event.target.value);
+    }
+  }
+
   private onClick = () => {
+    this.updateGraph(this.state.temp);
+  }
+
+  private updateGraph = (dot: string) => {
     try {
-      read(this.state.temp);
-      this.props.onUpdate(this.state.temp);
+      read(dot);
+      this.props.onUpdate(dot);
       this.setState({
         error: ''
       })
@@ -40,6 +56,7 @@ export class GraphInput extends React.Component<GraphInputProps, GraphInputState
 }
 
 interface GraphInputState {
+  autoUpdate: boolean,
   dot: string,
   error: string,
   temp: string,
