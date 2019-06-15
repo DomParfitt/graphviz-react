79d5c24d7d2716a8b02feee6e653859d8ea548b3
DomParfitt <dom.parfitt@gmail.com>
2019-06-15 22:25:31 +0100

    Added demo build

---
+++ docs/build/precache-manifest.36904f42b738ed91a00b4f7cf5b69eee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebce9d6658a95e608ce1005d68392398",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f5f69a1626b6cde6874",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "2dd40bf0847db52dfb30",
+    "url": "/static/js/2.6c194476.chunk.js"
+  },
+  {
+    "revision": "7f5f69a1626b6cde6874",
+    "url": "/static/js/main.e11e230c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
