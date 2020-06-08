363c6262d02194a70134f0e1a7008eb6c69236d1
Dom Parfitt <mail@domparfitt.com>
2020-06-08 14:47:28 +0100

    Added wasm script

--- src/Graphviz.tsx
+++ src/Graphviz.tsx
@@ -3,58 +3,61 @@ import * as React from 'react';
 
 export class Graphviz extends React.Component<IGraphvizProps, any> {
 
-    private static count = 0;
-    private static defaultOptions: GraphvizOptions = {
-        fit: true,
-        height: 500,
-        width: 500,
-        zoom: false,
-    };
+  private static count = 0;
+  private static defaultOptions: GraphvizOptions = {
+    fit: true,
+    height: 500,
+    width: 500,
+    zoom: false,
+  };
 
-    private id: string;
+  private id: string;
 
-    constructor(props: IGraphvizProps) {
-        super(props);
-        this.id = "graphviz" + Graphviz.count;
-        Graphviz.count++;
+  constructor(props: IGraphvizProps) {
+    super(props);
+    this.id = "graphviz" + Graphviz.count;
+    Graphviz.count++;
+  }
+
+  public render(): JSX.Element {
+    return (
+      <div>
+        <script src="https://unpkg.com/@hpcc-js/wasm/dist/index.min.js" type="application/javascript/"></script>
+        <div id={this.id} />
+      </div>
+    );
+  }
+
+  public componentDidMount() {
+    this.renderGraph();
+  }
+
+  public componentDidUpdate() {
+    this.renderGraph();
+  }
+
+  private renderGraph() {
+    graphviz('#' + this.id)
+      .options(this.options())
+      .renderDot(this.props.dot);
+  }
+
+  private options(): GraphvizOptions {
+    if (!this.props.options) {
+      return Graphviz.defaultOptions
     }
 
-    public render(): JSX.Element {
-        return (
-            <div id={this.id} />
-        );
+    const options: GraphvizOptions = Graphviz.defaultOptions;
+    for (const option of Object.keys(this.props.options)) {
+      options[option] = this.props.options[option];
     }
 
-    public componentDidMount() {
-        this.renderGraph();
-    }
-
-    public componentDidUpdate() {
-        this.renderGraph();
-    }
-
-    private renderGraph() {
-        graphviz('#' + this.id)
-            .options(this.options())
-            .renderDot(this.props.dot);
-    }
-
-    private options(): GraphvizOptions {
-        if (!this.props.options) {
-            return Graphviz.defaultOptions
-        }
-
-        const options: GraphvizOptions = Graphviz.defaultOptions;
-        for(const option of Object.keys(this.props.options)) {
-            options[option] = this.props.options[option];
-        }
-
-        return options;
-    }
+    return options;
+  }
 
 }
 
 export interface IGraphvizProps extends React.ClassAttributes<Graphviz> {
-    dot: string,
-    options?: GraphvizOptions,
+  dot: string,
+  options?: GraphvizOptions,
 }
