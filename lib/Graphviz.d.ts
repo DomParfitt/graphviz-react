754c09d277f3abfdce4aba72ffba781a29ddc4b1
Dom Parfitt <mail@domparfitt.com>
2020-06-12 22:01:05 +0100

    Rebuilt

--- lib/Graphviz.d.ts
+++ lib/Graphviz.d.ts
@@ -1,16 +1,17 @@
 import * as React from 'react';
 import { GraphvizOptions } from 'd3-graphviz';
 export declare class Graphviz extends React.Component<IGraphvizProps, any> {
-  private static count;
-  private static defaultOptions;
-  private id;
-  constructor(props: IGraphvizProps);
-  render: () => JSX.Element;
-  componentDidMount: () => void;
-  componentDidUpdate: () => void;
-  private renderGraph;
+    private static count;
+    private static defaultOptions;
+    private id;
+    constructor(props: IGraphvizProps);
+    render: () => JSX.Element;
+    componentDidMount: () => void;
+    componentDidUpdate: () => void;
+    private renderGraph;
+    private appendWasmScript;
 }
 export interface IGraphvizProps extends React.ClassAttributes<Graphviz> {
-  dot: string;
-  options?: GraphvizOptions;
+    dot: string;
+    options?: GraphvizOptions;
 }
