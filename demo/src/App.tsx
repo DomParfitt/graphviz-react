6aba62277c89bd2a81b337a4a1d6e4e4429d8de3
Dom Parfitt <mail@domparfitt.com>
2020-06-19 22:52:52 +0100

    Used Grid component in App

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -3,6 +3,7 @@ import type { GraphvizOptions } from 'd3-graphviz';
 import { Graphviz } from 'graphviz-react';
 import React, { useState } from 'react';
 import { GraphInput, OptionsSelector, Options } from './components';
+import Grid from './components/Grid';
 
 const defaults: Options<GraphvizOptions> = {
   convertEqualSidedPolygons: false,
@@ -30,37 +31,20 @@ const App = () => {
   const [dot, setDot] = useState('graph { a }');
   const [graphOptions, setGraphOptions] = useState(defaults);
 
-  const parent: React.CSSProperties = {
-    display: 'flex',
-    flexWrap: 'wrap',
-  };
-
-  const child: React.CSSProperties = {
-    flexGrow: 1,
-    flexShrink: 0,
-    flexBasis: '30%',
-  };
-
   return (
     <>
       <h1 style={{ textAlign: 'center' }}>Graphviz-React</h1>
-      <div style={parent}>
-        <div style={child}>
-          <GraphInput initialDot={dot} onUpdate={(newDot) => setDot(newDot)} />
-        </div>
-        <div style={child}>
-          <OptionsSelector
-            options={defaults}
-            onChange={(name, value) =>
-              setGraphOptions({ ...graphOptions, [name]: value })
-            }
-            allowedValues={allowedValues}
-          />
-        </div>
-        <div style={child}>
-          <Graphviz dot={dot} options={graphOptions} />
-        </div>
-      </div>
+      <Grid>
+        <GraphInput initialDot={dot} onUpdate={(newDot) => setDot(newDot)} />
+        <OptionsSelector
+          options={defaults}
+          onChange={(name, value) =>
+            setGraphOptions({ ...graphOptions, [name]: value })
+          }
+          allowedValues={allowedValues}
+        />
+        <Graphviz dot={dot} options={graphOptions} />
+      </Grid>
     </>
   );
 };
