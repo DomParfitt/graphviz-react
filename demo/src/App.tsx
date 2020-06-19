caf72f4547b25abb8a124d52b0fcc91c172b2c32
Dom Parfitt <mail@domparfitt.com>
2020-06-19 22:24:31 +0100

    Refactored options

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -5,41 +5,26 @@ import React, { useState } from 'react';
 import { GraphInput, OptionsSelector, Options } from './components';
 
 const defaults: Options<GraphvizOptions> = {
-  height: 550,
-  width: 550,
-  fit: true,
-  zoom: false,
-  scale: 1,
-  tweenPrecision: 1,
+  convertEqualSidedPolygons: false,
   engine: 'dot',
+  fade: false,
+  growEnteringEdges: false,
+  fit: true,
+  keyMode: 'title',
+  height: 550,
+  scale: 1,
+  tweenPaths: false,
+  tweenPrecision: 1,
+  tweenShapes: false,
+  useWorker: false,
+  width: 550,
+  zoom: false,
 };
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const options = [
-  { name: 'useWorker', type: 'boolean' },
-  {
-    name: 'engine',
-    type: 'list',
-    values: ['circo', 'dot', 'fdp', 'neato', 'osage', 'patchwork', 'twopi'],
-    default: defaults.engine,
-  },
-  {
-    name: 'keyMode',
-    type: 'list',
-    values: ['title', 'id', 'tag-index', 'index'],
-  },
-  { name: 'fade', type: 'boolean' },
-  { name: 'tweenPaths', type: 'boolean' },
-  { name: 'tweenShapes', type: 'boolean' },
-  { name: 'convertEqualSidedPolygons', type: 'boolean' },
-  { name: 'tweenPrecision', type: 'number', default: defaults.tweenPrecision },
-  { name: 'growEnteringEdges', type: 'boolean' },
-  { name: 'zoom', type: 'boolean', default: defaults.zoom },
-  { name: 'width', type: 'number', default: defaults.width },
-  { name: 'height', type: 'number', default: defaults.height },
-  { name: 'scale', type: 'number', default: defaults.scale },
-  { name: 'fit', type: 'boolean', default: defaults.fit },
-];
+const allowedValues = {
+  engine: ['circo', 'dot', 'fdp', 'neato', 'osage', 'patchwork', 'twopi'],
+  keyMode: ['title', 'id', 'tag-index', 'index'],
+};
 
 const App = () => {
   const [dot, setDot] = useState('graph { a }');
@@ -69,17 +54,7 @@ const App = () => {
             onChange={(name, value) =>
               setGraphOptions({ ...graphOptions, [name]: value })
             }
-            allowedValues={{
-              engine: [
-                'circo',
-                'dot',
-                'fdp',
-                'neato',
-                'osage',
-                'patchwork',
-                'twopi',
-              ],
-            }}
+            allowedValues={allowedValues}
           />
         </div>
         <div style={child}>
