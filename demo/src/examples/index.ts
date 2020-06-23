6910cb9c06e37790470729bcc292ec30a135930d
Dom Parfitt <mail@domparfitt.com>
2020-06-23 11:19:30 +0100

    Added extra examples

--- demo/src/examples/index.ts
+++ demo/src/examples/index.ts
@@ -14,6 +14,44 @@ const examples = {
     d -> e
     e -> a
 }`,
+  different_edge_types: `digraph D {
+
+    A [shape=diamond]
+    B [shape=box]
+    C [shape=circle]
+  
+    A -> B [style=dashed, color=grey]
+    A -> C [color="black:invis:black"]
+    A -> D [penwidth=5, arrowhead=none]
+  
+  }`,
+  nested_clusters: `digraph D {
+
+    subgraph cluster_p {
+      label = "Parent";
+  
+      subgraph cluster_c1 {
+        label = "Child one";
+        a;
+  
+        subgraph cluster_gc_1 {
+          label = "Grand-Child one";
+           b;
+        }
+        subgraph cluster_gc_2 {
+          label = "Grand-Child two";
+            c;
+            d;
+        }
+  
+      }
+  
+      subgraph cluster_c2 {
+        label = "Child two";
+        e;
+      }
+    }
+}`,
 };
 
 export default examples;
