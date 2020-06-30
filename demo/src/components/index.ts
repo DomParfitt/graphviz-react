7996280d596d8f7a128a0d86081be1d979f42808
Dom Parfitt <mail@domparfitt.com>
2020-06-30 15:43:34 +0100

    Fixed exports

--- demo/src/components/index.ts
+++ demo/src/components/index.ts
@@ -1,3 +1,5 @@
 export * from './GraphInput';
+export { default as Graphviz } from './Graphviz';
+export { default as Grid } from './Grid';
 export * from './OptionsSelector';
 export * from './TabbedContainer';
