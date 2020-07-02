d6b59ecf0e92f0fd4b8b8fe0695824fb9c336bdb
Dom Parfitt <mail@domparfitt.com>
2020-07-02 15:59:23 +0100

    Updated theme picker

--- demo/src/components/ThemePicker.tsx
+++ demo/src/components/ThemePicker.tsx
@@ -1,34 +1,58 @@
 import React, { useState } from 'react';
-import styled, { ThemeProvider } from 'styled-components';
+import styled, { ThemeProvider, createGlobalStyle } from 'styled-components';
 import theme from 'styled-theming';
 import { lighten } from 'polished';
 
-const bgColor = theme('mode', {
+const buttonBgColor = theme('mode', {
   dark: lighten(0.25, '#282c34'),
 });
 
 const Button = styled.button`
-  background-color: ${bgColor};
+  background-color: ${buttonBgColor};
+  position: absolute;
   :focus {
     outline: none;
   }
 `;
 
-const ThemePicker = ({ children }: { children: React.ReactNode }) => {
-  const [theme, setTheme] = useState('dark');
+const Container = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+`;
+
+const bgColor = theme('mode', {
+  dark: '#282c34',
+});
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: ${bgColor};
+  }
+`;
+
+const ThemePicker = ({
+  children,
+  themes,
+}: {
+  children: React.ReactNode;
+  themes: [string, ...string[]];
+}) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const theme = themes[currentIndex];
+
+  const cycleThemes = () => setCurrentIndex((currentIndex + 1) % themes.length);
 
   return (
     <ThemeProvider theme={{ mode: theme }}>
-      <Button
-        type="button"
-        onClick={() =>
-          theme === 'dark' ? setTheme('light') : setTheme('dark')
-        }
-      >
-        {`${theme.charAt(0).toUpperCase()}${theme.slice(1)}`}
-      </Button>
+      <GlobalStyle />
+      <Container>
+        <Button type="button" onClick={cycleThemes}>
+          {`${theme.charAt(0).toUpperCase()}${theme.slice(1)}`}
+        </Button>
+      </Container>
       {children}
     </ThemeProvider>
   );
 };
+
 export default ThemePicker;
