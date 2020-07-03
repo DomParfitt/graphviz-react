8cd141607f16f0051cd13289ef0ca1e4dcb25dbc
Dom Parfitt <mail@domparfitt.com>
2020-07-03 13:28:33 +0100

    Tweaked theme

--- demo/src/themes/dark.ts
+++ demo/src/themes/dark.ts
@@ -1,44 +1,47 @@
 import { lighten } from 'polished';
 import { Theme } from './theme';
 
+const colors = {
+  black: '#000000',
+  grey: '#282c34',
+  white: '#abb2bf',
+  blue: '#61afef',
+  red: '#be5046',
+  green: '#98c379',
+};
+
 const base = {
   backgroundColor: '#282c34',
   textColor: '#abb2bf',
+  hoverColor: '#61afef',
 };
 
 const graphArea = {
   backgroundColor: '#4b5263',
 };
 
-const graphInput = {
-  backgroundColor: base.backgroundColor,
-  textColor: base.textColor,
-  hoverColor: '#61afef',
-};
+// const graphInput = {
+//   hoverColor: '#61afef',
+// };
 
 const optionsSelector = {
-  backgroundColor: base.backgroundColor,
-  textColor: base.textColor,
   input: {
     backgroundColor: lighten(0.4, base.backgroundColor),
-    textColor: 'black',
+    // textColor: 'black',
   },
 };
 
 const tabContainer = {
   backgroundColor: lighten(0.25, base.backgroundColor),
-  textColor: 'black',
-  hoverColor: '#61afef',
   inactiveTabs: {
     backgroundColor: lighten(0.4, base.backgroundColor),
-    textColor: 'black',
   },
 };
 
 const dark: Theme = {
   ...base,
   graphArea,
-  graphInput,
+  // graphInput,
   optionsSelector,
   tabContainer,
 };
