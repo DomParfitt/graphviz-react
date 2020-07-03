491547bf33d47fa9413a324ff1da628980b87e40
Dom Parfitt <mail@domparfitt.com>
2020-07-03 21:51:09 +0100

    Added light theme

---
+++ demo/src/themes/light.ts
@@ -0,0 +1,47 @@
+import { lighten, darken } from 'polished';
+import styled from 'styled-components';
+import GraphvizTheme from './graphviz';
+
+// styled.div`
+//   background-color: pap;
+// `;
+
+const colors = {
+  black: '#000000',
+  white: '#ffffff',
+  green: 'green',
+  red: 'red',
+  darkestGrey: 'grey',
+  darkGrey: 'darkgrey',
+  lightGrey: 'lightgrey',
+};
+
+const light: GraphvizTheme = {
+  backgroundColor: colors.white,
+  textColor: colors.black,
+  hoverColor: colors.darkestGrey,
+  graphInput: {
+    autoUpdate: {
+      backgroundColor: colors.green,
+      inactive: {
+        backgroundColor: colors.red,
+      },
+    },
+    updateButton: {
+      backgroundColor: colors.lightGrey,
+    },
+  },
+  optionsSelector: {
+    input: {
+      backgroundColor: colors.lightGrey,
+    },
+  },
+  tabContainer: {
+    backgroundColor: colors.darkGrey,
+    inactiveTabs: {
+      backgroundColor: colors.lightGrey,
+    },
+  },
+};
+
+export default light;
