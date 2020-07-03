9566da590fcbfb35283de8e47437911ad1cd0dd5
Dom Parfitt <mail@domparfitt.com>
2020-07-03 15:38:43 +0100

    Documentation

--- demo/src/themes/theme.ts
+++ demo/src/themes/theme.ts
@@ -2,18 +2,28 @@ export interface ThemeElements {
   backgroundColor?: string;
   textColor?: string;
   hoverColor?: string;
+  hoverTextColor?: string;
 }
 
-interface NestedThemeElements {
+export interface NestedThemeElements {
   [key: string]: ThemeElements | NestedThemeElements;
 }
 
 export type Theme = ThemeElements | NestedThemeElements;
 
-interface ThemeMap {
+export interface ThemeMap {
   [key: string]: Theme;
 }
 
+/**
+ * Gets the value for a particular theme property from a given theme.
+ * Searches down the given `paths` in the `theme` object and returns the deepest
+ * instance of the given `property`.
+ * Returns an empty string if no value can be found.
+ * @param theme the theme to get the property for
+ * @param property the property to get, e.g. `backgroundColor`
+ * @param paths the paths to search down
+ */
 function getPropertyForTheme(
   theme: Theme,
   property: keyof ThemeElements,
@@ -37,6 +47,14 @@ function getPropertyForTheme(
   );
 }
 
+/**
+ * Gets the value for a particular theme property for all given themes.
+ * Searches down the given `paths` in the `theme` objects and returns the deepest
+ * instance of the given `property`.
+ * @param theme the theme to get the property for
+ * @param property the property to get, e.g. `backgroundColor`
+ * @param paths the paths to search down
+ */
 export const getPropertyForThemes = (
   themes: ThemeMap,
   property: keyof ThemeElements,
