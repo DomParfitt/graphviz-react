8b612228cee416df646ec3c4449acf7f505caf54
Dom Parfitt <mail@domparfitt.com>
2020-07-03 13:28:27 +0100

    Added helper

--- demo/src/themes/index.ts
+++ demo/src/themes/index.ts
@@ -1,8 +1,10 @@
-import { createContext } from 'react';
+import theme from 'styled-theming';
 import dark from './dark';
+import { getPropertyForThemes, ThemeElements } from './theme';
 
 export const themes = {
   dark,
 };
 
-export const ThemeContext = createContext(themes.dark);
+export const getProperty = (property: keyof ThemeElements, paths?: string[]) =>
+  theme('mode', getPropertyForThemes(themes, property, paths));
