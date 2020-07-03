3476cd34801276524b1e4c6a07e93c631f461069
Dom Parfitt <mail@domparfitt.com>
2020-07-03 21:55:10 +0100

    Began adding theme

---
+++ demo/src/themes/blue.ts
@@ -0,0 +1,58 @@
+import { lighten, darken } from 'polished';
+import styled from 'styled-components';
+import GraphvizTheme from './graphviz';
+
+// styled.div`
+//   background-color: pap;
+// `;
+
+const colors = {
+  black: '#000000',
+  white: '#ffffff',
+  darkBlue: '#1D3557',
+  blue: '#457B9D',
+  red: '#E63946',
+  green: '#2F7275',
+};
+
+const light: GraphvizTheme = {
+  backgroundColor: colors.white,
+  textColor: colors.black,
+  hoverColor: lighten(0.1, colors.blue),
+  hoverTextColor: colors.black,
+  graphArea: {
+    // backgroundColor: colors.primary,
+  },
+  graphInput: {
+    // backgroundColor: colors.primary,
+    autoUpdate: {
+      backgroundColor: colors.green,
+      inactive: {
+        backgroundColor: colors.red,
+      },
+    },
+    updateButton: {
+      backgroundColor: colors.blue,
+      hoverColor: colors.darkBlue,
+      hoverTextColor: colors.white,
+    },
+  },
+  optionsSelector: {
+    backgroundColor: colors.blue,
+    textColor: colors.white,
+    input: {
+      backgroundColor: colors.darkBlue,
+      textColor: colors.white,
+    },
+  },
+  tabContainer: {
+    backgroundColor: colors.darkBlue,
+    textColor: colors.white,
+    inactiveTabs: {
+      backgroundColor: colors.blue,
+      textColor: colors.white,
+    },
+  },
+};
+
+export default light;
