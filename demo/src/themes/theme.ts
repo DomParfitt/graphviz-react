735c0aa26b462c6693b2f3f871a0d480b9d0dae7
Dom Parfitt <mail@domparfitt.com>
2020-07-03 13:28:18 +0100

    Redefined theme and added helpers

--- demo/src/themes/theme.ts
+++ demo/src/themes/theme.ts
@@ -1,40 +1,49 @@
-type ThemeValue = string;
-
-export interface Theme {
-  backgroundColor: ThemeValue;
-  textColor: ThemeValue;
-  graphArea: {
-    backgroundColor: ThemeValue;
-  };
-  graphInput: {
-    backgroundColor: ThemeValue;
-    textColor: ThemeValue;
-    hoverColor: ThemeValue;
-  };
-  optionsSelector: {
-    backgroundColor: ThemeValue;
-    textColor: ThemeValue;
-    input: {
-      backgroundColor: ThemeValue;
-      textColor: ThemeValue;
-    };
-  };
-  tabContainer: {
-    backgroundColor: ThemeValue;
-    textColor: ThemeValue;
-    hoverColor: ThemeValue;
-    inactiveTabs: {
-      backgroundColor: ThemeValue;
-      textColor: ThemeValue;
-    };
-  };
+export interface ThemeElements {
+  backgroundColor?: string;
+  textColor?: string;
+  hoverColor?: string;
 }
+
+interface NestedThemeElements {
+  [key: string]: ThemeElements | NestedThemeElements;
+}
+
+export type Theme = ThemeElements | NestedThemeElements;
 
 interface ThemeMap {
   [key: string]: Theme;
 }
 
-export const getElement = (themes: ThemeMap, path: keyof Theme) =>
+function getPropertyForTheme(
+  theme: Theme,
+  property: keyof ThemeElements,
+  paths: string[] = []
+): string {
+  const currentValue = (theme[property] as string) || '';
+
+  if (paths.length === 0) {
+    return currentValue;
+  }
+
+  const path = paths[0];
+  const nextTheme = theme[path as keyof Theme];
+  if (!nextTheme) {
+    return currentValue;
+  }
+
+  return (
+    getPropertyForTheme(nextTheme as Theme, property, paths.slice(1)) ||
+    currentValue
+  );
+}
+
+export const getPropertyForThemes = (
+  themes: ThemeMap,
+  property: keyof ThemeElements,
+  paths?: string[]
+) =>
   Object.entries(themes)
-    .map(([key, value]) => ({ [key]: value[path] }))
+    .map(([key, value]) => ({
+      [key]: getPropertyForTheme(value, property, paths),
+    }))
     .reduce((prev, curr) => ({ ...prev, ...curr }), {});
